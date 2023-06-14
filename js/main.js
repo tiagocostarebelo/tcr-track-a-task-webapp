b5a6fc69d40da6d3c8b22ed9a1c2aa4123f28763
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2023-06-14 21:25:20 +0100

    updated styling. initiated the logic to store tasks in localstorage.

--- js/main.js
+++ js/main.js
@@ -18,12 +18,10 @@ addTaskBtn.addEventListener('click', addNewTask);
 window.addEventListener('click', deleteTask);
 window.addEventListener('click', editTask);
 
-
-
 // FUNCTIONS
 function setUsername() {
     if(nameInput.value == '') {
-        nameInput.style.backgroundColor = "lightblue";
+        nameInput.style.backgroundColor = "#d8f2fb";
     } else {
         nameInput.style.backgroundColor = "transparent";
         username = nameInput.value;
@@ -34,7 +32,7 @@ function setUsername() {
 
 function checkLocalStorage(username, tasks) {
     username = localStorage.getItem('username');
-    // tasks = JSON.parse(localStorage.getItem('tasks')) || []; 
+    tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
     
     if(username !== '') {
         nameInput.value = username;
@@ -51,10 +49,55 @@ function adjustInputWidth(username) {
 function addNewTask(event) {
     event.preventDefault();
     newTask = taskInput.value;
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
     tasks.push(newTask); 
     updateUi(newTask);
     updateOpenTasks(tasks);
     taskInput.value = '';
+}
+
+function editTask(e) {
+    const clickedBtn = e.target;
+    if(clickedBtn.classList.contains('edit-btn')) {
+        const inputField = clickedBtn.parentElement.firstChild;
+        const oldTaskName = inputField.value;
+        inputField.removeAttribute("readonly");
+        inputField.style.backgroundColor = "#d8f2fb";
+        inputField.focus();
+        
+        inputField.addEventListener('blur', (e) => {
+            const newTaskName = inputField.value;
+            inputField.setAttribute('readonly', true);
+            inputField.style.backgroundColor = "transparent";
+            if(tasks.includes(oldTaskName)) {
+                const indexOfTasks = tasks.indexOf(oldTaskName);
+                tasks[indexOfTasks] = newTaskName;
+            }
+        })
+    }
+    else {
+        return;
+    }
+}
+
+function deleteTask(e) {
+    const clickedBtn = e.target;
+    if(clickedBtn.classList.contains('delete-btn')) {
+        const taskName = clickedBtn.parentElement.firstChild.value;
+        if (tasks.includes(taskName)) {
+            const indexOfTasks = tasks.indexOf(taskName);
+            tasks.splice(indexOfTasks, 1);
+        }
+        clickedBtn.parentElement.remove();
+        updateOpenTasks(tasks);
+    }
+    else {
+        return;
+    }
+}
+
+function updateOpenTasks(tasks) {   
+    count.innerHTML = tasks.length;     
 }
 
 function updateUi(newTask) {
@@ -83,49 +126,6 @@ function updateUi(newTask) {
     tasksContainer.appendChild(fragment);
 }
 
-function updateOpenTasks(tasks) {   
-    count.innerHTML = tasks.length;     
-}
-
-function deleteTask(e) {
-    const clickedBtn = e.target;
-    if(clickedBtn.classList.contains('delete-btn')) {
-        const taskName = clickedBtn.parentElement.firstChild.value;
-        if (tasks.includes(taskName)) {
-            const indexOfTasks = tasks.indexOf(taskName);
-            tasks.splice(indexOfTasks, 1);
-        }
-        clickedBtn.parentElement.remove();
-        updateOpenTasks(tasks);
-    }
-    else {
-        return;
-    }
-}
-
-function editTask(e) {
-    const clickedBtn = e.target;
-    if(clickedBtn.classList.contains('edit-btn')) {
-        const inputField = clickedBtn.parentElement.firstChild;
-        const oldTaskName = inputField.value;
-        inputField.removeAttribute("readonly");
-        inputField.style.backgroundColor = "lightblue";
-        inputField.focus();
-        
-        inputField.addEventListener('blur', (e) => {
-            const newTaskName = inputField.value;
-            inputField.style.backgroundColor = "transparent";
-            inputField.setAttribute('readonly', true);
-            if(tasks.includes(oldTaskName)) {
-                const indexOfTasks = tasks.indexOf(oldTaskName);
-                tasks[indexOfTasks] = newTaskName;
-            }
-        })
-    }
-    else {
-        return;
-    }
-}
 
 
 
